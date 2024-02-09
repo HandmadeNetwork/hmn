c5e458be8c77254afe4d3ba99a806dd9606b2dc9
Ben Visness <ben@bvisness.me>
2024-02-09 15:02:58 -0600

    Lay out top section

--- src/website/routes.go
+++ src/website/routes.go
@@ -61,7 +61,7 @@ func NewWebsiteRoutes(conn *pgxpool.Pool) http.Handler {
 
 	hmnOnly.GET(hmnurl.RegexJamsIndex, JamsIndex)
 	hmnOnly.GET(hmnurl.RegexJamIndex, func(c *RequestContext) ResponseData {
-		return c.Redirect(hmnurl.BuildJamIndex2023(), http.StatusFound)
+		return c.Redirect(hmnurl.BuildJamIndex2024_Learning(), http.StatusFound)
 	})
 	hmnOnly.GET(hmnurl.RegexJamIndex2021, JamIndex2021)
 	hmnOnly.GET(hmnurl.RegexJamIndex2022, JamIndex2022)
