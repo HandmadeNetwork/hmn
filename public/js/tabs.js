22e7ad7a0cbc272a55d84b5722e8949870004905
Ben Visness <bvisness@gmail.com>
2026-07-25 12:00:07 -0500

    Add tabs to project settings (and unify use of tab utilities)

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,14 +1,40 @@
+/**
+ * A utility for a tabbed section on the page. To use it:
+ * 
+ *  - Add the `data-tab="foo"` attribute to any items you want to show and
+ *    hide.
+ *  - Add the `data-tab-button="foo"` attribute to any links you want to act as
+ *    tab buttons. (It is expected that these links will have the `tab-button`
+ *    class.)
+ *  - Call this function.
+ * 
+ * The tab buttons will then show and hide the tab elements using the HTML
+ * `hidden` attribute. The active tab button will also get the
+ * `tab-button-active` class.
+ * 
+ * You can provide the following options in the second argument:
+ * 
+ *  - `initialTab`: A string identifying a tab to be automatically selected.
+ *  - `onSelect`: A function of type `(name: string) => void` that is triggered
+ *    when a new tab is selected.
+ * 
+ * Returns an object with the following:
+ * 
+ *  - `selectTab(name, options?: { sendEvent?: bool = true })`:
+ *    A function that selects a tab by name without clicking a button. By
+ *    setting `sendEvent` to false you can suppress the call to `onSelect`.
+ */
 function initTabs(container, {
     initialTab = null,
-    onSelect = (name) => {},
-}) {
+    onSelect = name => {},
+} = {}) {
     const buttons = Array.from(container.querySelectorAll("[data-tab-button]"));
     const tabs = Array.from(container.querySelectorAll("[data-tab]"));
 
     const firstTab = tabs[0].getAttribute("data-tab");
 
     function selectTab(name, { sendEvent = true } = {}) {
-        if (!document.querySelector(`[data-tab="${name}"]`)) {
+        if (!container.querySelector(`[data-tab="${name}"]`)) {
             console.warn("no tab found with name", name);
             return selectTab(firstTab, initial);
         }
@@ -35,4 +61,27 @@ function initTabs(container, {
     return {
         selectTab,
     };
+}
+
+/**
+ * A wrapper around the above that automatically uses the URL #hash.
+ */
+function initHashTabs(container, {
+    initialTab = null,
+} = {}) {
+    const res = initTabs(container, {
+        initialTab: initialTab ?? document.location.hash.substring(1),
+        onSelect(name) {
+            document.location.hash = `#${name}`;
+        },
+    });
+    const { selectTab } = res;
+    window.addEventListener("hashchange", e => {
+        const tab = new URL(e.newURL).hash.substring(1);
+        if (tab) {
+            selectTab(tab, { sendEvent: false });
+        }
+    });
+    
+    return res;
 }
